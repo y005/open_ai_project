0bf18d702a12697f9a527b91830af9aa4a698a26
문정현 <jhmoon@miridih.com>
2022-12-11 11:50:06 +0900

    초기 설정

---
+++ src/main/java/com/example/open_ai_project/OpenAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.open_ai_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAiProjectApplication.class, args);
+    }
+
+}
